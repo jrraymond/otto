63d5b80e4dbabf620129587077bf613fc1f32535
Justin Raymond <justin.raymond.14@gmail.com>
2015-12-01 23:31:22 -0500

    heterogenous lists of tests

--- otto.0.0.1/src/otto.ml
+++ otto.0.0.1/src/otto.ml
@@ -1,7 +1,11 @@
 
 
 (*** tests ***)
-type 'a output = Pass of 'a | Fail of 'a | Exn of exn | Timeout
+type output =
+  | Pass of string
+  | Fail of string
+  | Group of string * output list
+
 
 (* we cant do this because we want heterogeneous test suites.
  * Instead we use a list of functions : Unit -> Unit.
@@ -11,45 +15,16 @@ type 'a output = Pass of 'a | Fail of 'a | Exn of exn | Timeout
  * log parameter, which is responsible for printing the output. *)
 (* I guess the next question I have is how do I count pass/fails.
  * I think the logger function may have some internal state maybe? *)
-type ('a, 'b, 'c) test = 
-  { msg : string
-  ; args : 'a
-  ; ans : 'c
-  ; time : int
-  ; func : 'a -> 'b
-  ; eq : 'b -> 'c -> bool
-  ; status : 'b output }
 
-let tgroup ?msg:(m="") ?eq_fun:(eq=(=)) ?timeout:(t=60) tests = ();;
-
-let log m r args outp ans = ();;
-let logm m = ();;
-let utest ?msg:(m="") ?eq_fun:(eq=(=)) ?timeout:(t=60) func args ans = 
-  fun () -> let outp = func args in log m (eq outp ans) args outp ans; ()
-
-let ugroup msg tests = fun () -> logm msg; List.map (fun f -> f ()) tests
-
-(*
-let otest_unit msg f eq (inp,ans) =
-  let f = fun () -> (try OK (f inp) with e -> Exn e) in
-  let outp = (match timeout f () 1 None with
-              | Some r -> r
-              | None -> Exn TIMEOUT) in
-  {msg = msg;inp = inp;outp = outp;ans = ans;pass = eq outp ans}
-*)  
-let test_list = ugroup [ utest ~msg:"inc" (fun x -> x + 1) 0 1;
-                         utest (fun s -> s ^ "!") "!" "!!" ]
-
-
+type ('a, 'b) either = Left of 'a | Right of 'b
 
 (* kills process, smothers error if process does not exist *)
 let kill pid sign = 
   try Unix.kill pid sign with
   | Unix.Unix_error (e,f,p) -> ()
   | e -> raise e;;
-  
+
 (* runs f on arg and stops after time seconds if not completed, returns defualt if timeout.
- * TODO compare before marshalling
  * *)
 let timeout f arg time default = 
   let pipe_r,pipe_w = Unix.pipe () in
@@ -71,6 +46,8 @@ let timeout f arg time default =
                  let result = (Marshal.from_channel ic : 'b option) in
                  result ));;
 
+let catcher func args = try Right (func args) with e -> Left e;;
+
 (* basic testing function
  * msg : message string to associate with the test
  * f : function to apply to the input
@@ -79,25 +56,33 @@ let timeout f arg time default =
  * ans : expected output
  * returns report
 *)
-let otest_unit msg f eq (inp,ans) =
-  let f = fun () -> (try OK (f inp) with e -> Exn e) in
-  let outp = (match timeout f () 1 None with
-              | Some r -> r
-              | None -> Exn TIMEOUT) in
-  {msg = msg;inp = inp;outp = outp;ans = ans;pass = eq outp ans}
-  
+(* TODO compare before marshalling *)
+let otest ?msg:(m="") ?eq_fun:(eq=(=)) ?timeout:(t=60) func args ans = fun () ->
+  if m = "" then () else Printf.printf "%s\n" m; flush stdout;
+  (match timeout (catcher func) args t None with
+              | None -> Printf.printf "[FAILED] timed out after %i seconds\n" t; flush stdout
+              | Some (Left e) -> Printf.printf "[FAILED] Exception %s\n" (Printexc.to_string e); flush stdout
+              | Some (Right e) when eq e ans -> Printf.printf "[PASS]\n"; flush stdout
+              | _ -> Printf.printf "[FAIL]\n"); flush stdout
 
-let otester msg f eq lst =
-  let results = List.map (fun pair -> otest_unit msg f eq pair) lst in
-  let passed = List.length (List.filter (fun rpt -> rpt.pass) results) in
-  let failed = List.length results - passed in
-  (passed,failed,results)
+let tgroup ?msg:(m="") ?eq_fun:(eq=(=)) ?timeout:(t=60) tests =
+  fun () -> Printf.printf "%s\n" m; flush stdout; List.map (fun f -> f ()) tests
+
+
+    
+
+let run_tests suite = suite ()
+
+let test_list = tgroup ~msg:"arithmetic"
+  [ otest ~msg:"inc" ~timeout:1 (fun x -> x + 1) 0 1;
+    otest ~timeout:1 (fun s -> s ^ "!") "!" "!!" ]
+
+let _ = run_tests test_list
 
 (* report to string
  * fi : function to turn the input into a string
  * fo : function to turn the output/answer into a string
  * rpt : the report
- *)
 let report_to_str fi fo rpt =
   let i_str = fi rpt.inp in
   let o_str = fo rpt.outp in
@@ -106,7 +91,8 @@ let report_to_str fi fo rpt =
   then "[PASS] "^rpt.msg^"\t"^i_str^" -> "^o_str ^"\n"
   else "[FAIL] "^rpt.msg^"\n\tgot: "^o_str^"\n\texpected: "^a_str^"\n\tinput:"^i_str^"\n"
 
-(* lifts an equality function to the level of type output *)
+ *)
+(* lifts an equality function to the level of type output
 let output_eq eq_fun out ans =
   match out with
   | OK tout ->
@@ -117,7 +103,27 @@ let output_eq eq_fun out ans =
       (match ans with
       | Exn e2 -> e1 = e2
       | _ -> false)
-  
+*) 
 
 let print_pf p f = 
   Printf.printf "%i PASSED, %i FAILED\n\n" p f; flush stdout;;
+(*
+let string_of_output out0 =
+  let rec go indent accum out =
+    let spaces = String.make indent ' ' in
+    (match out with
+     | Pass s -> (1, 0, accum ^ "\n" ^ spaces ^ s)
+     | Fail s -> (0, 1, accum ^ "\n" ^ spaces ^ s)
+     | Group (m, ts) ->
+         let (p, f, s) = List.fold_right
+          (fun t (pac, fac, sac) ->
+            let (p, f, s) = go (indent + 1) sac t in
+            (pac + p, fac + f, sac ^ s))
+          ts
+          (0, 0, accum) 
+            in spaces ^ m ^ "\n"
+            (pa + p, fa + f)) (0, 0, ) ts in
+         (p, f, Printf.sprintf "%s%s [%i PASSED, %i FAILED]\n%s" spaces m p f s)) in
+  let (p, f, s) = go 0 "" out0 in
+  (p, f, s)
+*)
--- otto.0.0.1/src/otto.mli
+++ otto.0.0.1/src/otto.mli
@@ -1,24 +1,16 @@
-type 'a output = OK of 'a | Exn of exn
-type ('a, 'b, 'c) report = {
-  msg : string;
-  inp : 'a;
-  outp : 'b;
-  ans : 'c;
-  pass : bool;
-}
-exception TIMEOUT
+type output = Pass of string | Fail of string | Group of string * output list
+type ('a, 'b) either = Left of 'a | Right of 'b
 val kill : int -> int -> unit
 val timeout : ('a -> 'c) -> 'a -> int -> 'd -> 'b option
-val otest_unit :
-  string ->
-  ('a -> 'b) ->
-  ('c output -> 'd -> bool) -> 'a * 'd -> ('a, 'e output, 'd) report
-val otester :
-  string ->
-  ('a -> 'b) ->
-  ('c output -> 'd -> bool) ->
-  ('a * 'd) list -> int * int * ('a, 'e output, 'd) report list
-val report_to_str :
-  ('a -> string) -> ('b -> string) -> ('a, 'b, 'b) report -> string
-val output_eq : ('a -> 'b -> bool) -> 'a output -> 'b output -> bool
+val catcher : ('a -> 'b) -> 'a -> (exn, 'b) either
+val otest :
+  ?msg:string ->
+  ?eq_fun:('a -> 'a -> bool) ->
+  ?timeout:int -> ('b -> 'c) -> 'b -> 'a -> unit -> unit
+val tgroup :
+  ?msg:string ->
+  ?eq_fun:('a -> 'a -> bool) ->
+  ?timeout:int -> (unit -> 'b) list -> unit -> 'b list
+val run_tests : (unit -> 'a) -> 'a
+val test_list : unit -> unit list
 val print_pf : int -> int -> unit
