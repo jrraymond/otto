6f452c7f1d7d91e547ee93841807c66e462dd3b3
Justin Raymond <justin.raymond.14@gmail.com>
2015-12-14 17:40:03 -0500

    refined interface

--- src/otto.mli
+++ src/otto.mli
@@ -1,19 +1,7 @@
-val const : 'a -> 'b -> 'a
 val logger_fun : (string -> unit) ref
 val default_logger : string -> unit
-val pass_fail_count : (int * int) list ref
-val pop_pass_fail_count : unit -> int * int
-val pop_and_add_pass_fail_count : unit -> int * int
-val push_pass_fail_count : int * int -> unit
-val inc_pass_count : unit -> unit
-val inc_fail_count : unit -> unit
-val print_counts : unit -> unit
 type ('a, 'b) either = Left of 'a | Right of 'b
 val from_either : ('a -> 'b) -> ('c -> 'b) -> ('a, 'c) either -> 'b
-val kill : int -> int -> unit
-val timeout : ('a -> 'c) -> 'a -> int -> 'd -> 'b option
-val catcher :
-  ('a -> 'b) -> ((exn, 'b) either -> 'c) -> 'a -> (exn, 'b) either * 'c
 val test :
   ?msg:string ->
   ?timeout:int ->
